1908b23353e1820e023ff6ef462012c08ff64df4
kendall motes <kmotes@reliaquest.com>
2019-12-07 18:11:24 -0700

    Initial commit

---
+++ src/main/java/com/example/identity/user/UserController.java
@@ -0,0 +1,12 @@
+package com.example.identity.user;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RestController
+public class UserController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}

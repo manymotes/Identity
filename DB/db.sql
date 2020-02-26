7c406c16301129048848762a8bbaf20a4cd6a256
kendall motes <kmotes@reliaquest.com>
2020-02-26 09:31:06 -0700

    db change

--- DB/db.sql
+++ DB/db.sql
@@ -1,11 +1,11 @@
 CREATE TABLE users (
     uuid uuid PRIMARY KEY,
-    first_name VARCHAR (255) NOT NULL,
-    last_name VARCHAR (255) NOT NULL,
-    password VARCHAR (255) NOT NULL,
-    email VARCHAR (255) NOT NULL UNIQUE,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    password TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
     age INTEGER,
-    gender VARCHAR (50)
+    gender TEXT
 );
 
 CREATE TABLE sessions(
